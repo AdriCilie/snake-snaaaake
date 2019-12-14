651dd74d0e57cce899cc950034ff181c4fcd093f
Pavel Yurchenkov <youonlyplaytwice@gmail.com>
2019-12-14 15:21:39 +0300

    Game entry point
    Added main game settings which include main camera, playground, and snake.Also added scripts (GameHandler, GameAssets) which is game entry point

---
+++ Snake/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.instance.snakeHeadSprite;
+    }
+
+    void Update()
+    {
+    }
+}
